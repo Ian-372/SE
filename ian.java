1bc63b01cccf1e1710c5ca0511aecad3ab084858
Ian-372 <ianmutuli36@gmail.com>
2026-09-14 10:48:00 +0300

    java mastery-intro to java basics lesson 01

---
+++ ian.java
@@ -0,0 +1,26 @@
+//java learning for my software engineering journey
+public class ian {
+    public static void main(String[] args) {
+        //initializing variables and constants in java
+        //variables
+         String name= "ian";
+         int age = 20 ;
+         double height = 1.6;
+         //constant
+         final String COUNTRY = "KENYA"; //MEANS const cannot be changed (final)
+         //printing the values of the variables and constant
+         //conditions
+         if (height < 1.8)
+            {
+                System.out.println("You are a short little cunt");
+
+            }
+            else {
+                System.out.println("You are tall enough.");
+            }
+
+
+
+        System.out.println( "my name is " + name + ", and i'm  " + age + " years old " + ", with a  height of " + height + "meters " +", coming  from " + COUNTRY);
+    }
+}
